5fe58a5bc7c89129e2ffd6be2bd63018d6866a2c
Iryna Pochepynets <pochepynetsiryna@gmail.com>
2024-11-29 15:58:15 +0200

    Session 4

--- src/main/java/org/prog/collections/HomeWorkCollections.java
+++ src/main/java/org/prog/collections/HomeWorkCollections.java
@@ -11,7 +11,50 @@ package org.prog.collections;
 // > ...
 // > ...
 
+import org.prog.cars.Car;
+
+import java.util.*;
+
 // >>>> Car1: Jane, Bill
 // >>>> Car4: John, Sarah
 public class HomeWorkCollections {
+    public static void main(String[] args) {
+        Map<Owner, List<String>> ownersAndCar = new HashMap<>();
+        String requestedCar = "Car 1";
+        Set<Owner> ownersOfSpecificCars = findsOwner(ownersAndCar, requestedCar);
+        addOwnersToCompany(ownersAndCar);
+        addOwnersToCompany(ownersAndCar);
+        addOwnersToCompany(ownersAndCar);
+        addOwnersToCompany(ownersAndCar);
+
+        for (Owner owner : ownersOfSpecificCars) {
+            System.out.println(owner);
+        }
+    }
+
+    public static void addOwnersToCompany(Map<Owner, List<String>> ownersAndCar) {
+        Owner owner = new Owner();
+        ownersAndCar.put(owner, new ArrayList<>());
+        ownersAndCar.get(owner).add("Jane");
+        ownersAndCar.get(owner).add("John");
+        ownersAndCar.get(owner).add("Bill");
+    }
+
+    public static Set<Owner> findsOwner(Map<Owner, List<String>> ownersAndCar, String requestedCar) {
+        Set<Owner> ownersOfSpecificCars = new HashSet<>();
+        Iterator<Map.Entry<Owner, List<String>>> iterator =
+                ownersAndCar.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Owner, List<String>> entry = iterator.next();
+            Owner ownerVerified = entry.getKey();
+            List<String> car = entry.getValue();
+            if (car.contains(requestedCar)) {
+                ownersOfSpecificCars.add(ownerVerified);
+            }
+        }
+
+        return ownersOfSpecificCars;
+    }
 }
+
+
---
+++ src/main/java/org/prog/collections/Owner.java
@@ -0,0 +1,10 @@
+package org.prog.collections;
+
+public class Owner {
+    public String name;
+
+    @Override
+    public String toString() {
+        return name + "it owner";
+    }
+}
